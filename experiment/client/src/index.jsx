6b92873923f533b1d8c37a3dc14d64c152ed8f3d
Alicia Chen <amqchen@gmail.com>
2026-02-18 15:37:58 -0500

    add sentry

--- experiment/client/src/index.jsx
+++ experiment/client/src/index.jsx
@@ -5,11 +5,36 @@ import "virtual:uno.css";
 import "../node_modules/@empirica/core/dist/player.css";
 import App from "./App";
 import "./index.css";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://82d1223301f5ea7d0c6d07f9935aeabd@o4510902672359424.ingest.us.sentry.io/4510908813541376",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      maskAllInputs: false,
+      blockAllMedia: false,
+    }),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 1.0, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.,
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+});
 
 const container = document.getElementById("root");
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
